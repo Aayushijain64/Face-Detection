8b0c2c8b9928d521f65e3dd59fff0bce653e3856
Aayushi Jain <37550379+Aayushijain64@users.noreply.github.com>
2019-08-17 20:12:37 +0530

    Add files via upload

---
+++ facial_landmarks.py
@@ -0,0 +1,81 @@
+ # USAGE
+# python facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_01.jpg 
+
+# import the necessary packages
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+	help="path to facial landmark predictor")
+args = vars(ap.parse_args())
+
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# load the input image, resize it, and convert it to grayscale
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("OUTPUT")
+
+img_counter = 0
+
+while True:
+    ret, image = cam.read()
+    cv2.imshow("output", image)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, image)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+image = imutils.resize(image, width=500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# detect faces in the grayscale image
+rects = detector(gray, 1)
+
+# loop over the face detections
+for (i, rect) in enumerate(rects):
+	# determine the facial landmarks for the face region, then
+	# convert the facial landmark (x, y)-coordinates to a NumPy
+	# array
+	shape = predictor(gray, rect)
+	shape = face_utils.shape_to_np(shape)
+
+	# convert dlib's rectangle to a OpenCV-style bounding box
+	# [i.e., (x, y, w, h)], then draw the face bounding box
+	(x, y, w, h) = face_utils.rect_to_bb(rect)
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+	# show the face number
+	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+	# loop over the (x, y)-coordinates for the facial landmarks
+	# and draw them on the image
+	for (x, y) in shape:
+		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+# show the output image with the face detections + facial landmarks
+cv2.imshow("Output", image)
+cv2.waitKey(0)
